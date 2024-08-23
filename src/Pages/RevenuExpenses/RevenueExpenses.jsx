2991fa1852006622796168b5d015e0a2b2dd6cb5
OsamaBarakaat <osaamabarakaat@gmail.com>
2024-08-23 03:01:09 +0300

    revenue and expenses design update

--- src/Pages/RevenuExpenses/RevenueExpenses.jsx
+++ src/Pages/RevenuExpenses/RevenueExpenses.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import { Modal, Button, Tab, Tabs, Table } from "react-bootstrap";
+import {
+  Modal,
+  Button,
+  Tab,
+  Tabs,
+  Table,
+  FloatingLabel,
+  Form,
+} from "react-bootstrap";
 import { Bar } from "react-chartjs-2";
 import { FaDollarSign, FaHandHoldingUsd } from "react-icons/fa";
 import "./RevenueExpenses.css";
+import { Plus, Trash2Icon } from "lucide-react";
 
 const dummyData = {
   income: [
@@ -22,16 +31,19 @@ const dummyData = {
 const RevenueExpenses = () => {
   const [revenue, setRevenue] = useState(dummyData.income);
   const [expenses, setExpenses] = useState(dummyData.expenses);
-  const [showModal, setShowModal] = useState(false);
+  const [showModalRevenue, setShowModalRevenue] = useState(false);
+  const [showModalExpense, setShowModalExpense] = useState(false);
   const [isRevenueModal, setIsRevenueModal] = useState(true);
   const [key, setKey] = useState("all");
 
-  const handleShowModal = (isRevenue) => {
-    setIsRevenueModal(isRevenue);
-    setShowModal(true);
+  const handleShowModalRevenue = () => {
+    setShowModalRevenue(true);
   };
-
-  const handleCloseModal = () => setShowModal(false);
+  const handleShowModalExpense = () => {
+    setShowModalExpense(true);
+  };
+  const handleCloseModalRevenue = () => setShowModalRevenue(false);
+  const handleCloseModalExpense = () => setShowModalExpense(false);
 
   const addTransaction = (e) => {
     e.preventDefault();
@@ -43,10 +55,11 @@ const RevenueExpenses = () => {
     };
     if (isRevenueModal) {
       setRevenue([...revenue, newTransaction]);
+      handleCloseModalRevenue();
     } else {
       setExpenses([...expenses, newTransaction]);
+      handleCloseModalExpense();
     }
-    handleCloseModal();
   };
 
   const data = {
@@ -66,20 +79,61 @@ const RevenueExpenses = () => {
     ],
   };
 
+  const expenseTitles = ["Rent", "Food", "Maintenance", "Bill", "Other"];
+
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [formData, setFormData] = useState(
+    expenseTitles.reduce(
+      (acc, title) => {
+        acc[title] = { cost: "", rebete: "" };
+        return acc;
+      },
+      { extra: { title: "", cost: "", rebete: "" } }
+    )
+  );
+
+  const handleCardClick = (card) => {
+    setSelectedCards((prevSelected) =>
+      prevSelected.includes(card)
+        ? prevSelected.filter((selectedCard) => selectedCard !== card)
+        : [...prevSelected, card]
+    );
+  };
+
+  const handleInputChange = (e, card) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [card]: { ...prevData[card], [name]: value },
+    }));
+  };
+
+  const handleAddClick = () => {
+    const filledCards = Object.keys(formData)
+      .filter(
+        (card) =>
+          formData[card].title || formData[card].cost || formData[card].rebete
+      )
+      .map((card) => ({ card, ...formData[card] }));
+  };
+
   return (
     <div className="m-4">
       <div className="contOfCardsAndChart mb-4">
         <div className="CardsOfContOne mb-4">
           <div
             className="bigCard adadada w-100 h-100 cursor-pointer"
-            onClick={() => handleShowModal(true)}
+            onClick={() => handleShowModalRevenue(true)}
           >
             <h5 className="card-title d-flex justify-content-start mb-3">
               Income
             </h5>
             <div className="card-body d-flex justify-content-center align-items-center flex-column h-75">
-              <div className="roundeddd roundeddd-one">
+              <div className="roundeddd roundeddd-one plusHidden">
                 <FaHandHoldingUsd size={40} className="mb-2" color="#396AFF" />
+              </div>
+              <div className="roundeddd roundeddd-one hiddenPlus">
+                <Plus size={40} className="mb-2" color="#396AFF" />
               </div>
 
               <h4 className="mt-3">$3,700</h4>
@@ -89,14 +143,17 @@ const RevenueExpenses = () => {
         <div className="CardsOfContTwo mb-4">
           <div
             className=" bigCard sdsdsdsd w-100 h-100 cursor-pointer"
-            onClick={() => handleShowModal(false)}
+            onClick={() => handleShowModalExpense(true)}
           >
             <h5 className="card-title d-flex justify-content-start mb-3">
               Expense
             </h5>
             <div className="card-body d-flex justify-content-center align-items-center flex-column h-75">
-              <div className="roundeddd roundeddd-two">
+              <div className="roundeddd roundeddd-two plusHiddenTwo">
                 <FaDollarSign size={40} className="mb-2" color="#FF82AC" />
+              </div>
+              <div className="roundeddd roundeddd-one hiddenPlusTwo">
+                <Plus size={40} className="mb-2" color="#FF82AC" />
               </div>
 
               <h4 className="mt-3">$1,260</h4>
@@ -128,44 +185,190 @@ const RevenueExpenses = () => {
         </Tab>
       </Tabs>
 
-      <Modal show={showModal} onHide={handleCloseModal}>
+      <Modal show={showModalRevenue} onHide={handleCloseModalRevenue} size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>
-            {isRevenueModal ? "Add Revenue" : "Add Expense"}
-          </Modal.Title>
+          <Modal.Title>Add New Income</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form onSubmit={addTransaction}>
-            <div className="form-group">
-              <label>Description</label>
-              <input
-                name="description"
-                type="text"
-                className="form-control"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Amount</label>
-              <input
-                name="amount"
-                type="text"
-                className="form-control"
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mt-3">
-              Add
-            </button>
-          </form>
+          <div className="cardsInRevenue">
+            {Object.keys(formData).map((card, index) => (
+              <div
+                key={index}
+                className={`card ${
+                  selectedCards.includes(card) ? "selected" : ""
+                }`}
+                onClick={() => handleCardClick(card)}
+              >
+                <div className="cardHead">
+                  <div className="flexcentercenter">
+                    <div className="cardHeadRounded">
+                      <FaHandHoldingUsd
+                        size={40}
+                        className="mb-2"
+                        color="#396AFF"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="cardBody">
+                  <div className="w-100 mb-3">
+                    <FloatingLabel
+                      controlId={`floatingInput${index}`}
+                      label="Title"
+                    >
+                      <Form.Control
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={formData[card].title}
+                        onChange={(e) => handleInputChange(e, card)}
+                      />
+                    </FloatingLabel>
+                  </div>
+                  <div className="w-100 mb-3">
+                    <FloatingLabel
+                      controlId={`floatingCost${index}`}
+                      label="Cost"
+                    >
+                      <Form.Control
+                        type="text"
+                        placeholder="Cost"
+                        name="cost"
+                        value={formData[card].cost}
+                        onChange={(e) => handleInputChange(e, card)}
+                      />
+                    </FloatingLabel>
+                  </div>
+                  <FloatingLabel
+                    controlId={`floatingRebate${index}`}
+                    label="Rebate"
+                    className="mt-3"
+                  >
+                    <Form.Select
+                      placeholder="Rebate"
+                      name="rebate"
+                      value={formData[card].rebate}
+                      onChange={(e) => handleInputChange(e, card)}
+                    >
+                      <option value="" disabled selected>
+                        Select Rebate
+                      </option>
+                      <option value={"daily"}>Daily</option>
+                      <option value={"Weekly"}>Weekly</option>
+                      <option value={"monthly"}>Monthly</option>
+                      <option value={""}>No Rebate</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                </div>
+              </div>
+            ))}
+          </div>
         </Modal.Body>
+        <Modal.Footer className="my-2">
+          <Button variant="secondary" onClick={handleCloseModalRevenue}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAddClick}>
+            Add Income
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showModalExpense} onHide={handleCloseModalExpense} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Add New Expense</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="cardsInRevenue">
+            {Object.keys(formData).map((card, index) => (
+              <div
+                key={index}
+                className={`card ${
+                  selectedCards.includes(card) ? "selected" : ""
+                }`}
+                onClick={() => handleCardClick(card)}
+              >
+                <div className="cardHead">
+                  <div className="flexcentercenter">
+                    <div className="cardHeadRounded">
+                      <FaDollarSign
+                        size={40}
+                        className="mb-2"
+                        color="#FF82AC"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="cardBody">
+                  {card === "extra" ? (
+                    <FloatingLabel
+                      controlId={`floatingInput${index}`}
+                      label="Title"
+                    >
+                      <Form.Control
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        value={formData[card].title}
+                        onChange={(e) => handleInputChange(e, card)}
+                      />
+                    </FloatingLabel>
+                  ) : (
+                    <h5 className="card-title">{card}</h5>
+                  )}
+                  <FloatingLabel
+                    controlId={`floatingCost${index}`}
+                    label="Cost"
+                    className="mt-3"
+                  >
+                    <Form.Control
+                      type="text"
+                      placeholder="Cost"
+                      name="cost"
+                      value={formData[card].cost}
+                      onChange={(e) => handleInputChange(e, card)}
+                    />
+                  </FloatingLabel>
+                  <FloatingLabel
+                    controlId={`floatingRebate${index}`}
+                    label="Rebate"
+                    className="mt-3"
+                  >
+                    <Form.Select
+                      placeholder="Rebate"
+                      name="rebate"
+                      value={formData[card].rebate}
+                      onChange={(e) => handleInputChange(e, card)}
+                    >
+                      <option value="" disabled selected>
+                        Select Rebate
+                      </option>
+                      <option value={"daily"}>Daily</option>
+                      <option value={"Weekly"}>Weekly</option>
+                      <option value={"monthly"}>Monthly</option>
+                      <option value={""}>No Rebate</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="my-2">
+          <Button variant="secondary" onClick={handleCloseModalExpense}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAddClick}>
+            Add Expense
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
 };
 
 const TransactionTable = ({ data }) => (
-  <Table striped bordered hover>
+  <Table striped bordered hover responsive>
     <thead>
       <tr>
         <th>Description</th>
@@ -175,26 +378,14 @@ const TransactionTable = ({ data }) => (
       </tr>
     </thead>
     <tbody>
-      {data.map((transaction, index) => (
-        <tr>
-          <td>{transaction.description}</td>
-          <td>{transaction.date}</td>
-          <td>{transaction.amount}</td>
-          <td className="d-flex justify-content-center align-items-center">
+      {data.map((item, index) => (
+        <tr key={index}>
+          <td>{item.description}</td>
+          <td>{item.date}</td>
+          <td>{item.amount}</td>
+          <td className="d-flex justify-content-center">
             <Button variant="danger">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-trash"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                </svg>
-              </span>
+              <Trash2Icon size={20} />
             </Button>
           </td>
         </tr>
