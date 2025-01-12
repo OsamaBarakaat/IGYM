abbc6a52c9caae24d3c92a58077b9fd80869ab88
Osama Barakat <osaamabarakaat@gmail.com>
2025-01-12 20:26:57 +0200

    add dates in revenu page

--- src/utils/FormatDate.js
+++ src/utils/FormatDate.js
@@ -1,16 +1,7 @@
 export const formatDate = (dateString) => {
     const date = new Date(dateString);
 
-    const day = date.getUTCDate();
-    const month = date.getUTCMonth() + 1; // Months are zero-indexed
-    const year = date.getUTCFullYear();
+    const localString = date.toLocaleString();
 
-    let hours = date.getUTCHours(); // Get hours in UTC
-    const minutes = date.getUTCMinutes().toString().padStart(2, "0"); // Ensure two digits
-    const ampm = hours >= 12 ? "PM" : "AM";
-
-    // Convert to 12-hour format
-    hours = hours % 12 || 12;
-
-    return `${day}-${month}-${year}, ${hours}:${minutes} ${ampm}`;
+    return localString;
 };
